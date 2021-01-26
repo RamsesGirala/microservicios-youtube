d72006968a2e3c7200b9ecd5c8467af73be3d615
RamsesGirala <ramsesgiralald@gmail.com>
2021-01-26 16:50:57 -0300

    agregado gateway

--- gateway-service/src/main/java/academy/digitallab/store/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/academy/digitallab/store/gateway/GatewayServiceApplication.java
@@ -1,18 +1,15 @@
-package academy.digitallab.store.shopping;
+package academy.digitallab.store.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
 
 @EnableEurekaClient
 @SpringBootApplication
-@EnableFeignClients
-public class ServiceShoppingApplication {
+public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceShoppingApplication.class, args);
+		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
 }
